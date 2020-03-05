b43150da3b05cac104824f99823ee45ff6f081d1
Jorge Luis Villamizar Arenales <jorge.villamizar@labcomp.unet.edu.ve>
2020-03-05 11:56:14 -0300

    añadiendo

--- main.cpp
+++ main.cpp
@@ -4,5 +4,44 @@
 
 int main(int argc, char** argv) {
 	//Fase I
+	//Nuevos auxiliares ios::out
+	/*
+	Datos.dat
+	172 daniel
+	120 luis
+	315 maria
+	216 jose
+	125 tomas
+	160 juan
+	190 carolina
+	217 andrea
+	200 felix
+	
+	*/
+	//Aux1.dat 		 aux2.dat
+	/*
+	172 daniel		120 luis
+	315 maria		216 jose
+	125 tomas		160 juan
+	190 carolina	217 andrea
+	200 felix		
+	
+		5 reg			4 reg
+	
+	b)Cerramos todos los archivos
+	c)abrir pero cambiando el proposito o modo
+		
+		ios::in
+	aux1.dat		ayx2.dat		datos.dat
+	172 daniel		120 luis		
+	315 maria		216 jose
+	125 tomas		160 juan
+	190 carolina	217 andrea
+	200 felix
+	
+	
+	
+	
+	*/
 	return 0;
 }
